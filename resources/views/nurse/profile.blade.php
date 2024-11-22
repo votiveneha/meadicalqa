39e106a8f4d7ad144dca61e523ba88db3a6bf29d
votiveneha <votivephp.neha@gmail.com>
2024-11-22 10:36:10 +0530

    Update profile.blade.php

--- resources/views/nurse/profile.blade.php
+++ resources/views/nurse/profile.blade.php
@@ -7,7 +7,7 @@
 <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.5.1/uicons-regular-rounded/css/uicons-regular-rounded.css'>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
-
+hello
 <style type="text/css">
   .hide_profile_image{
     display: none !important;
