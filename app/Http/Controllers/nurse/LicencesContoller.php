9742cad3a629e67efc878dff434134e6e061c898
votiveneha <votivephp.neha@gmail.com>
2025-05-22 18:51:13 +0530

    Update LicencesContoller.php

--- app/Http/Controllers/nurse/LicencesContoller.php
+++ app/Http/Controllers/nurse/LicencesContoller.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-
+use App\Models\LicensesModel;
 use Illuminate\Support\Facades\Log;
 use App\Services\User\AuthServices;
 use App\Http\Requests\UserUpdateProfile;
@@ -29,7 +29,9 @@ class LicencesContoller extends Controller{
 
     public function registration_licences()
     {
-        return view ("nurse.registration_licences");
+        $user_id = Auth::guard('nurse_middle')->user()->id;
+        $data['licenses_data'] = LicensesModel::where("user_id",$user_id)->first();
+        return view ("nurse.registration_licences")->with($data);
     }
 
     public function ahepra_lookup(Request $request)
@@ -77,6 +79,141 @@ class LicencesContoller extends Controller{
 
         return response()->json($result);
     }
+
+    public function update_registration_licenses(Request $request){
+        $ahpra_registration_status = $request->ahpra_registration_status;
+        $user_id = $request->user_id;
+
+        if($ahpra_registration_status == "RN" || $ahpra_registration_status == "RM" || $ahpra_registration_status == "RN_RM" || $ahpra_registration_status == "NP"){
+            $ahpra_number = $request->ahpra_number;
+            $ahpra_consent = isset($request->ahpra_consent)?1:0;
+            $division = $request->division;
+            $endorsements = $request->endorsements;
+            $registration_type = $request->reg_registration_type;
+            $registration_status = $request->reg_registration_status;
+            $notations = json_encode($request->notations);
+            $other_notation = $request->other_notation;
+            $conditions = json_encode($request->conditions);
+            $expiry_date = $request->expiry_date;
+            $principal_place = $request->principal_place;
+            $other_places = json_encode($request->other_places);
+            //$upload_register_evidence = $request->upload_register_evidence;
+
+            
+        }
+
+        $licenses_data = LicensesModel::where("user_id",$user_id)->first();
+        //print_r($licenses_data);die;
+        if(!empty($licenses_data)){
+
+            //$licenses_register = LicensesModel::find($user_id);
+            $run = LicensesModel::where('user_id',$user_id)->update([
+                'ahpra_registration_status'=>$ahpra_registration_status,
+                'aphra_verifying_checkbox'=>$ahpra_consent,
+                'aphra_registration_no'=>$ahpra_number,
+                'register_division'=>$division,
+                'register_endorsements'=>$endorsements,
+                'register_reg_type'=>$registration_type,
+                'register_reg_status'=>$registration_status,
+                'register_notations'=>$notations,
+                'register_conditions'=>$conditions,
+                'register_principal_place'=>$principal_place,
+                'register_other_place'=>$other_places,
+                'register_other_notation_reason'=>$other_notation,
+                'register_expiry'=>$expiry_date,
+            ]);
+            
+
+        }else{
+            
+            $licenses_register = new LicensesModel();
+            $licenses_register->user_id = $user_id;
+            $licenses_register->ahpra_registration_status = $ahpra_registration_status;
+            $licenses_register->aphra_verifying_checkbox = $ahpra_consent;
+            $licenses_register->aphra_registration_no = $ahpra_number;
+            
+            $licenses_register->register_division = $division;
+            $licenses_register->register_endorsements = $endorsements;
+            $licenses_register->register_reg_type = $registration_type;
+            $licenses_register->register_reg_status = $registration_status;
+            $licenses_register->register_notations = $notations;
+            $licenses_register->register_conditions = $conditions;
+            $licenses_register->register_principal_place = $principal_place;
+            $licenses_register->register_other_place = $other_places;
+            $licenses_register->register_other_notation_reason = $other_notation;
+            $licenses_register->register_expiry = $expiry_date;
+            //$licenses_register->register_upload_evidence = $user_id;
+            $run = $licenses_register->save();
+        }
+
+        if ($run) {
+            $json['status'] = 1;
+            
+        } else {
+            $json['status'] = 0;
+            
+        }
+
+        echo json_encode($json);
+
+        
+    }
+
+    public function uploadLicensesEvidenceImgs(Request $request){
+
+        $img_field = $request->img_field;
+        $files = $request->file("register_upload_evi");
+        //print_r($files);
+        $user_id = $request->user_id;
+
+        $getLicensesdata = LicensesModel::where("user_id", $user_id)->first();
+
+        if(!empty($getLicensesdata) && $getLicensesdata->register_upload_evidence != NULL){
+            $ev_img = (array)json_decode($getLicensesdata->register_upload_evidence);
+            print_r($ev_img);
+            $licensesimgs = Helpers::multipleFileUpload($files, $ev_img);
+        }else{
+            $licensesimgs = Helpers::multipleFileUpload($files, '');
+        }
+
+        
+
+        $run = LicensesModel::where('user_id', $user_id)->update(["register_upload_evidence" => $licensesimgs]);
+        return $licensesimgs;
+    }
+
+    public function deleteLicensesEvidenceImg(Request $request)
+    {
+        $img_field = $request->img_field;
+        $user_id = $request->user_id;
+        
+        $img = $request->img;
+
+        $getLicensesdata = LicensesModel::where("user_id", $user_id)->first();
+
+        if(!empty($getLicensesdata) && $getLicensesdata->register_upload_evidence != NULL){
+            $ev_img = (array)json_decode($getLicensesdata->register_upload_evidence);
+
+            $img_index = array_search($img, $ev_img);
+
+            array_splice($ev_img, $img_index, 1);
+
+            $deleteData = LicensesModel::where('user_id', $user_id)->update(["register_upload_evidence" => json_encode($ev_img)]);
+        
+            $destinationPath = public_path() . '/uploads/education_degree/' . $img;
+
+            if (File::exists($destinationPath)) {
+                File::delete($destinationPath);
+            }
+        }else{
+            $deleteData = 1;
+        }
+
+        if ($deleteData) {
+            return 1;
+        }
+
+    }    
 
 }
 
