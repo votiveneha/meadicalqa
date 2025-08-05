bf02adb06ce56d9f576b3bb19a9da45bf802253f
votiveneha <votivephp.neha@gmail.com>
2025-08-05 18:48:30 +0530

    Update JobsController.php

--- app/Http/Controllers/nurse/JobsController.php
+++ app/Http/Controllers/nurse/JobsController.php
@@ -45,6 +45,9 @@ class JobsController extends Controller{
         $data['type_of_nurse'] = DB::table("practitioner_type")
             ->where("parent", 0)
             ->get();        
+        $data['speciality'] = DB::table("speciality")
+            ->where("parent", 0)
+            ->get();            
         return view('nurse.find_jobs')->with($data);
     }
 
@@ -139,13 +142,40 @@ class JobsController extends Controller{
     {
         $nurse_id = $request->nurse_id;
         $nurse_data = DB::table("practitioner_type")->where("parent",$nurse_id)->get();
+        
         $ndatas = array();
         foreach($nurse_data as $ndata){
-            $ndatas['id'] = $ndata->id;
-            $ndatas['name'] = $ndata->name;
-            $ndatas['parent'] = $ndata->parent;
+            
+            $nsubarr = array();
+            $get_nurse_count = DB::table("practitioner_type")->where("parent",$ndata->id)->get();
+
+            foreach($get_nurse_count as $get_nurse){
+                $nsubarr[] = array("id"=>$get_nurse->id,"name"=>$get_nurse->name,"parent"=>$get_nurse->parent);
+            }
+            
+            $ndatas[] = array("id"=>$ndata->id,"name"=>$ndata->name,"parent"=>$ndata->parent,"get_nurse_count"=>count($get_nurse_count),"get_nurse"=>$nsubarr);
         }
-        return json_encode($nurse_data);
+        return json_encode($ndatas);
+    }
+
+    public function getSpecialityData(Request $request)
+    {
+        $specality_id = $request->speciality_id;
+        $specality_data = DB::table("speciality")->where("parent",$specality_id)->get();
+        
+        $specdatas = array();
+        foreach($specality_data as $specdata){
+            
+            $specsubarr = array();
+            $get_spec_count = DB::table("speciality")->where("parent",$specdata->id)->get();
+
+            foreach($get_spec_count as $get_spec){
+                $specsubarr[] = array("id"=>$get_spec->id,"name"=>$get_spec->name,"parent"=>$get_spec->parent);
+            }
+            
+            $specdatas[] = array("id"=>$specdata->id,"name"=>$specdata->name,"parent"=>$specdata->parent,"get_spec_count"=>count($get_spec_count),"get_spec"=>$specsubarr);
+        }
+        return json_encode($specdatas);
     }
 
 }
