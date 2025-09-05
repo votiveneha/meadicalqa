a01e6982ae63df66fc9ab549869a852c91411f9c
votiveneha <votivephp.neha@gmail.com>
2025-09-05 18:58:27 +0530

    Update nurse.php

--- routes/nurse.php
+++ routes/nurse.php
@@ -224,5 +224,6 @@ Route::prefix('nurse')->name('nurse.')->namespace('App\Http\Controllers\nurse')-
   Route::post('/getFilterSpecialityData', 'JobsController@getFilterSpecialityData')->name('getFilterSpecialityData');  
   Route::post('/updateSectorData', 'JobsController@updateSectorData')->name('updateSectorData');  
       Route::post('/getJobsSorting', 'JobsController@getJobsSorting')->name('getJobsSorting');
+      Route::post('/applyJobs', 'JobsController@applyJobs')->name('applyJobs');
   });
 });
